06edeebbad42bf95294bcba84d1fe196bd7e5f6d
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-10-21 21:09:28 -0400

    Update wildGeomEnv.R

--- Dev/wildGeomEnv.R
+++ Dev/wildGeomEnv.R
@@ -3,5 +3,9 @@ stop("Unfinished code")
 attach(name="wildGeomEnv", what=build.env.from.files(
 	  parent.name="package:stats",
 	  path="~/Mirrors/GitHub/dmparrishphd/Shapiro/Files/",
-	  subdirs=strsplit("7/4/0/line.pp.R", " ")[[1]]))
+	  files=strsplit(split="\n",
+'7/4/0/line.pp.R
+7/7/0/quadrance.m.R
+7/7/0/quadrance.R
+')[[1]]))
 })()
