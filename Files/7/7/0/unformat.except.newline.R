b1cf609d26533da7478714e2907399954dd1197a
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-10-21 20:48:07 -0400

    Create unformat.except.newline.R

---
+++ Files/7/7/0/unformat.except.newline.R
@@ -0,0 +1,11 @@
+unformat.except.newline
+=======================
+
+Details
+-------
+
+`unformat.except.newline` splits each element of the
+`character vector` argument by newline,
+consolidates the result into a single `character vector`,
+strips leading and trailing whitespace, and
+compresses all sequences ofwhitespace to a single space.
