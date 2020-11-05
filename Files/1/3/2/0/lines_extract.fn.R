fb0855c35e6b0c5e59310285446832872faff74c
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-11-05 10:32:25 -0500

    Update lines_extract.fn.R

--- Files/1/3/2/0/lines_extract.fn.R
+++ Files/1/3/2/0/lines_extract.fn.R
@@ -3,8 +3,3 @@ lines_extract.fn <- function(filename, i) {
     Return <- lines_extract.cn(f, i)
     f %|% close
     Return }
-
-    Doc$lines_extract.fn <- '
-        lines_extract.fn is similar to lines_extract.cn, except
-        that the name of a file is given (arg 1) rather than a
-        connection. Line 1 of the file corresponds with index 1.'
