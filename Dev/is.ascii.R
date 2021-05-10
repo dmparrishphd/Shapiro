e0f30565d785c86a0dc5493d44f46a73ed6efbf3
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-05-10 08:45:56 -0400

    Delete is.ascii.R

--- Dev/is.ascii.R
+++
@@ -1,7 +0,0 @@
-is.ascii <- function (.character) {
-    stop("UNTESTED")
-    vapply(.character, FUN.VALUE=T, function(x)
-        if (x %|% is.na %|% not && x %|% nchar) {
-            all(x %|% utf8ToInt) < 128
-        } else {
-            F } ) }
